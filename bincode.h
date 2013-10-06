934b0003b9d36ddd9a365cd47d2ce71735fef93b
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2013-10-06 13:59:49 +0400

    added binary code type

---
+++ bincode.h
@@ -0,0 +1,18 @@
+//
+//  bincode.h
+//  huffman
+//
+//  Created by ivan on 06.10.13.
+//  Copyright (c) 2013 dmitrievsky. All rights reserved.
+//
+
+#ifndef huffman_bincode_c
+#define huffman_bincode_c
+
+typedef struct BinCode
+{
+    long self;
+    int length;
+} BinCode;
+
+#endif
